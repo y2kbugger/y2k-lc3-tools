52a1a72064a735b8e410a4205d9367a1f3186ec2
y2k <git@y2kbugger.com>
2024-05-31 13:43:15 -0400

    Implement instruction reading for sqlvm and scaffold HLT

--- y2klc3tools/sqlvm.sql
+++ y2klc3tools/sqlvm.sql
@@ -3,12 +3,14 @@ INSERT INTO trace VALUES ('Reticulating splines...');
 CREATE TABLE signal(
     clk BOOLEAN,
     is_running BOOLEAN,
-    tracing BOOLEAN
+    tracing BOOLEAN,
+    instr INT
     );
 INSERT INTO signal VALUES (
     FALSE,
     FALSE,
-    FALSE
+    FALSE,
+    0
     );
 
 CREATE TABLE register(
@@ -34,7 +36,9 @@ CREATE TABLE memory(
 
 pragma recursive_triggers = 1;
 
+-- ################################
 -- CLK Rising edge detection
+-- ################################
 DROP TRIGGER IF EXISTS clk_trigger_rising;
 CREATE TRIGGER clk_trigger_rising
 AFTER UPDATE OF clk ON signal
@@ -44,7 +48,9 @@ BEGIN
     INSERT INTO trace VALUES ('clk rising edge');
 END;
 
+-- ################################
 -- CLK Falling edge detection
+-- ################################
 DROP TRIGGER IF EXISTS clk_trigger_falling;
 CREATE TRIGGER clk_trigger_falling
 AFTER UPDATE OF clk ON signal
@@ -53,7 +59,40 @@ BEGIN
     -- Insert into trace table based on clk edge detection
     INSERT INTO trace VALUES ('clk falling edge');
 
+    -- Read instruction from memory based on PC
+    -- There are two bytes the High and Low. We are Big Endian.
+    INSERT INTO trace VALUES ('reading instruction from memory...');
+    UPDATE signal
+        SET instr = (
+            (SELECT value FROM memory WHERE address = (SELECT PC FROM register)) << 8 |
+            (SELECT value FROM memory WHERE address = (SELECT PC FROM register) + 1)
+        );
+
     -- Update PC in register table based on falling edge detection
+    INSERT INTO trace VALUES ('incrementing PC...');
     UPDATE register
-        SET PC = PC + 1;
+        SET PC = PC + 2;
+END;
+
+-- ################################
+-- ## Instruction Handlers
+-- ################################
+
+-- ## Catchall
+DROP TRIGGER IF EXISTS instr_trigger;
+CREATE TRIGGER instr_trigger
+AFTER UPDATE OF instr ON signal
+BEGIN
+    -- Insert into trace table based on instruction update
+    INSERT INTO trace VALUES ('instruction updated');
+END;
+
+-- ## HLT ##
+DROP TRIGGER IF EXISTS instr_hlt_trigger;
+CREATE TRIGGER instr_hlt_trigger
+AFTER UPDATE OF instr ON signal
+    WHEN NEW.instr = 0xF025
+BEGIN
+    -- Insert into trace table based on instruction update
+    INSERT INTO trace VALUES ('HLT instruction detected');
 END;
